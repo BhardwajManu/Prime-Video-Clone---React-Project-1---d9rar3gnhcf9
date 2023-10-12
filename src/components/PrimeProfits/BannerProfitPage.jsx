d76e91b9bec1bb71dd2d69c2e987bcab8a59a211
BhardwajManu <bhabhardwajmamta@gmail.com>
2023-10-12 16:02:34 +0530

    build issue correct

--- src/components/PrimeProfits/BannerProfitPage.jsx
+++ src/components/PrimeProfits/BannerProfitPage.jsx
@@ -2,8 +2,8 @@ import Flickity from 'react-flickity-component'
 import primeprofit1 from '../../assets/images/BenefitBigImage1.jpeg';
 import primeprofit2 from '../../assets/images/benefitBigimg2.jpeg';
 import primeprofit3 from '../../assets/images/benefitBigImg3.jpeg';
-import primeprofit4 from '../../assets/images/benefitBigImg4.jpeg';
-import primeprofit5 from '../../assets/images/benefitBigImg5.jpeg';
+import primeprofit4 from '../../assets/images/BenefitBigImg4.jpeg';
+import primeprofit5 from '../../assets/images/BenefitBigImg5.jpeg';
 import "../../styles/bannerProfit.css";
 import primelogo1 from '../../assets/images/LoginLogo.png'
 import primelogo2 from '../../assets/images/primeBenefitLogo1.png'
